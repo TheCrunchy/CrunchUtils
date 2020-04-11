107d1f7d04f819e9f4f13ac147394f1b6cb87a27
TheCrunchy <crunchycamsta@gmail.com>
2020-04-12 00:16:14 +0100

    Should work TM

---
+++ FixPlayerCommand/Commands.cs
@@ -0,0 +1,73 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Ingame;
+using VRageMath;
+using Torch.Session;
+using Sandbox.Common;
+using Sandbox.Game;
+using Sandbox.Game.World;
+using Sandbox.Game.Entities;
+using VRage.Game.Entity;
+
+namespace FixPlayerPlugin
+{
+    public class Commands : CommandModule
+    {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
+        [Command("fixme", "Murder a player then respawn them at their current location")]
+        [Permission(MyPromoteLevel.None)]
+        public void CalcShipPrice()
+        {
+
+            IMyPlayer player = Context.Player;
+            long playerId;
+            if (player == null)
+            {
+                Context.Respond("Console cant do this");
+                return;
+            }
+            else
+            {
+                playerId = player.IdentityId;
+            }
+            try
+            {
+                MatrixD playerPos = player.Character.WorldMatrix;
+                VRage.ModAPI.IMyEntity entity = MyEntities.GetEntityById(player.Character.EntityId) as VRage.ModAPI.IMyEntity;
+                VRage.Game.ModAPI.IMyInventory playerInv = player.Character.GetInventory();
+                Dictionary<String, int> amounts = new Dictionary<string, int>();
+                //  MyItemType itemType = new MyInventoryItemFilter("MyObjectBuilder_Component/" + pair.Key).ItemType;
+
+                player.SpawnAt(playerPos, new Vector3(0, 0, 0), entity, false);
+                IMyCharacter oldCharacter = player.Character;
+                IMyCharacter newCharacter = player.Character;
+                oldCharacter.GetInventory().Clear();
+                oldCharacter.Kill();
+                oldCharacter.Delete();
+                MyEntities.Remove(MyEntities.GetEntityById(oldCharacter.EntityId));
+                player.SpawnIntoCharacter(newCharacter);
+                int i;
+                for (i = 0; i < playerInv.ItemCount; i++)
+                {
+                    playerInv.TransferItemTo(player.Character.GetInventory(), playerInv.GetItemAt(i).Value);
+                }
+                
+                Context.Respond("You should be fixed");
+            }
+            catch (Exception)
+            {
+                Context.Respond("Relog and try again");
+                return;
+            }
+            
+        }
+    }
+}
---
+++ FixPlayerCommand/FixPlayerPlugin.cs
@@ -0,0 +1,58 @@
+﻿using NLog;
+using Sandbox.Game.World;
+using System;
+using Sandbox.Game.GameSystems.BankingAndCurrency;
+using Torch;
+using Torch.API;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+using Torch.Session;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
+using System.Collections;
+using Torch.API.Managers;
+using Torch.API.Session;
+using System.IO;
+using Sandbox.Game.Entities;
+using VRage.Groups;
+using VRage.Game;
+using System.Collections.Concurrent;
+using Sandbox.Game.Entities.Cube;
+using System.Collections.Generic;
+
+namespace FixPlayerPlugin
+{
+    public class FixPlayerPlugin : TorchPluginBase
+    {
+
+        public static Logger Log = LogManager.GetCurrentClassLogger();
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+            Log.Info("Dirk hasnt gibben me the moneys :(");
+            var sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+
+            if (sessionManager != null)
+            {
+                sessionManager.SessionStateChanged += SessionChanged;
+            }
+            else
+            {
+                Log.Warn("No session manager loaded!");
+            }
+
+        }
+        private void SessionChanged(ITorchSession session, TorchSessionState newState)
+        {
+            //Do something here in the future
+            Log.Info("Session-State is now " + newState);
+            if (newState == TorchSessionState.Loaded)
+            {
+        
+            }
+
+        }
+    }
+}
